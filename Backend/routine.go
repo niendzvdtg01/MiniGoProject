21c946e7d46a35665bc1f84c90202f1a2ffe02e9
niendzvdtg01 <duynien34@gmail.com>
2026-05-22 23:27:31 +0700

    feat: add routine for concurrent job processing with worker pattern

---
+++ Backend/routine.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, result chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		fmt.Printf("worker %v xu ly job %v \n", id, job)
+		time.Sleep(time.Second)
+		result <- job + 1
+	}
+}
+
+func main() {
+	jobs := make(chan int, 10)
+	reusults := make(chan int, 10)
+	var wg sync.WaitGroup
+	for i := 1; i < 3; i++ {
+		wg.Add(1)
+		go worker(i, jobs, reusults, &wg)
+	}
+
+	for i := 1; i <= 9; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(reusults)
+	}()
+	for r := range reusults {
+		fmt.Println("Ket qua: ", r)
+	}
+}
--- Backend/test.go
+++ Backend/test.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	var cc string = "abc,cde,asjdja"
-	afterTest := strings.Split(cc, ",")
-	joinTest := strings.Join(afterTest, "")
-	fmt.Println(afterTest, joinTest)
-}
+// func main() {
+// 	var cc string = "abc,cde,asjdja"
+// 	afterTest := strings.Split(cc, ",")
+// 	joinTest := strings.Join(afterTest, "")
+// 	fmt.Println(afterTest, joinTest)
+// }
