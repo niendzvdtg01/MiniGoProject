612cbbff23ccfcc054a84cacbfe9b50d6ddbcba5
niendzvdtg01 <duynien34@gmail.com>
2026-05-26 22:42:59 +0700

    refactor: update worker function and main routine in routine.go; remove swap function in swap.go

--- Backend/swap.go
+++ Backend/swap.go
@@ -1,16 +1,1 @@
 package main
-
-import "fmt"
-
-func swap(a, b *int) (int, int) {
-	*a = *a + *b
-	*b = *a - *b
-	*a = *a - *b
-	return *a, *b
-}
-func main() {
-	a := 3
-	b := 4
-	swap(&a, &b)
-	fmt.Println(a, b)
-}
