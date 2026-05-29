9ebeb20a2e3b2498d9cadad950a40b967fd17c2c
niendzvdtg01 <duynien34@gmail.com>
2026-05-29 23:28:09 +0700

    Refactor code structure for improved readability and maintainability

--- Backend/internal/handler/NewsHandler.go
+++ Backend/internal/handler/NewsHandler.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"Backend/pkg/dto"
 	"Backend/pkg/utils"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,8 +26,31 @@ func (n *NewsHandler) PostNewsV1(ctx *gin.Context) {
 		})
 		return
 	}
+	img, err := ctx.FormFile("image")
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "file is a required",
+		})
+		return
+	}
+	//have permission
+	err = os.MkdirAll("./uploads", os.ModePerm)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Can not create folder!"})
+		return
+	}
+
+	dst := fmt.Sprintf("./uploads/%s", filepath.Base(img.Filename))
+
+	if err := ctx.SaveUploadedFile(img, dst); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Can not save file"})
+	}
+
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"name":   input.Title,
 		"Status": input.Status,
+		"img":    img.Filename,
+		"path":   dst,
 	})
 }
