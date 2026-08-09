d1df6d6e2a5d169428c5c063cb989fe43abbc6b2
niendzvdtg01 <duynien34@gmail.com>
2026-08-09 22:27:24 +0700

    feat:test migration

--- Backend/internal/db/migration/20260803145400_users.down.sql
+++ Backend/internal/db/migration/20260803145400_users.down.sql
@@ -1,1 +1,5 @@
-drop table if exists users;
+drop table if exists test123;
+
+migrate create -ext sql -dir internal/db/migration -seq users
+
+migrate -path internal/db/migration -database "postgresql://root:1234@localhost:5433/master-golang?sslmode=disable" up
--- Backend/internal/db/migration/20260803145400_users.up.sql
+++ Backend/internal/db/migration/20260803145400_users.up.sql
@@ -1,4 +1,4 @@
-create table users(
+create table test123(
     user_id serial primary key,
     name varchar(50),
     email varchar(100)
