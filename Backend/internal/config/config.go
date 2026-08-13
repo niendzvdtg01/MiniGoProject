60389041b6a14e3ee6213a4e070272f1e27432a1
niendzvdtg01 <duynien34@gmail.com>
2026-08-13 22:27:58 +0700

    feat: add DatabaseConfig struct and environment variable retrieval for database settings

--- Backend/internal/config/config.go
+++ Backend/internal/config/config.go
@@ -1,11 +1,39 @@
 package config
 
+import (
+	"backend/pkg/utils"
+)
+
 type Config struct {
 	ServeAddress string
+	DB           DatabaseConfig
+}
+
+//for db
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
 }
 
 func NewConfig() *Config {
 	return &Config{
 		ServeAddress: ":8085",
+		DB: DatabaseConfig{
+			Host:     utils.GetEnv("DB_HOST", "localhost"),
+			Port:     utils.GetEnv("DB_PORT", "5433"),
+			User:     utils.GetEnv("DB_USER", "postgres"),
+			Password: utils.GetEnv("DB_PASSWORD", "postgres"),
+			DBName:   utils.GetEnv("DB_NAME", "master-golang"),
+			SSLMode:  utils.GetEnv("DB_SSLMODE", "disable"),
+		},
 	}
+}
+
+func (c *Config) DSN() {
+
 }
